5e6a4aaf9ab77bd6cfb25eafe834884745d4e4e1
Lutong <lt@imlaidian.com>
2019-09-06 14:55:37 +0800

    添加接口调用测试方法

--- sbd-global-locks/src/test/java/com/lutfly/sbdGlobalLocks/TreasureTest.java
+++ sbd-global-locks/src/test/java/com/lutfly/sbdGlobalLocks/TreasureTest.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TreasureTest extends SbdGlobalLocksApplicationTests {
@@ -23,27 +22,31 @@ public class TreasureTest extends SbdGlobalLocksApplicationTests {
     @Test
     public void concurrentTest() throws InterruptedException {
 
-        String url = "http://127.0.0.1:8421//user-point/treasure/participateTreasure";
+        String url = "https://uo.imlaidian.com/user-point/treasure/participateTreasure";
 
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
-        header.set("Authorization", "tewdjkbu5e2pjkbxi3nr4zprk1uf4zprk1ua");
+        header.set("Authorization", "tewdBlbotexdnkhqi3nr4zprk1uf4");
         HttpEntity httpEntity = new HttpEntity(header);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
-                .queryParam("period", 2)
-                .queryParam("activityId", 1);
+                .queryParam("period", 9)
+                .queryParam("activityId", 6);
         URI uri = builder.buildAndExpand().toUri();
 
-        while (true) {
-            System.out.println("start");
-            for (int i = 0; i < 100; i++) {
-                new Thread(() -> {
-                    ResponseEntity<String> exchange = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
-                    log.error(exchange.getBody());
-                }).start();
-            }
-            TimeUnit.SECONDS.sleep(10000);
+//        while (true) {
+//            System.out.println("start");
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                ResponseEntity<String> exchange = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
+                log.error(exchange.getBody());
+            }).start();
+            new Thread(() -> {
+                ResponseEntity<String> exchange = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, String.class);
+                log.error(exchange.getBody());
+            }).start();
         }
+//        TimeUnit.SECONDS.sleep(10000);
+//        }
     }
 }
