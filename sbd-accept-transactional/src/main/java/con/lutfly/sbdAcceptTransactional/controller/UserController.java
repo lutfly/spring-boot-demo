54fef49decaa746b9b5313a8355df3feb19324e4
Lutong <lt@imlaidian.com>
2019-09-23 23:55:45 +0800

    cicd_test

--- sbd-accept-transactional/src/main/java/con/lutfly/sbdAcceptTransactional/controller/UserController.java
+++ sbd-accept-transactional/src/main/java/con/lutfly/sbdAcceptTransactional/controller/UserController.java
@@ -1,19 +1,14 @@
 package con.lutfly.sbdAcceptTransactional.controller;
 
-import con.lutfly.sbdAcceptTransactional.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @PostMapping("/")
-    public void test() throws InterruptedException {
-        userService.test();
+    @GetMapping("/")
+    public String test() throws InterruptedException {
+        return "hello world!";
     }
 
 }
